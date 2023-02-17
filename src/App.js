27753d39f3f060cab2f17350e2308bde806844db
Johndolf Isagani Magtibay <n01513815@humbermail.ca>
2023-02-17 07:43:51 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -19,17 +19,15 @@ const [resultText, setResultText] = useState('');
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 const getOpenAiResponse = () => {
   openai.createCompletion({
   model: "text-curie-001",
-  prompt: "You are a rant translator and your job is to \
-  translate and rephrase a user's rant to a more calmer, neutral, polite, \
-  and professional statements.You should reduce the risk of misunderstandings \
-  and help the use communicate their thoughts in \
-  a positive and effective way \n User Rant: " + inputText + 
+  prompt: "You are a chatbot trying to help a friend relay his rant \
+  to somebody in a polite way. Your job is tro translate the rant that \
+  will be received in a more positive light. \n User Rant: " + inputText + 
   "Translation:",
   max_tokens: 256,
   temperature: 0.7,
@@ -41,7 +39,7 @@ const getOpenAiResponse = () => {
   return (
     <div>
         <div className="app-header">
-            <h2 className="header">daddychill.ai</h2>
+            <h2 className="header">daddychill</h2>
             <h1 className="header2">The Rant Translator App</h1>
         </div>
         <div className="app-body">
@@ -49,10 +47,8 @@ const getOpenAiResponse = () => {
                     <Form>
                         <Form.Field
                             control={TextArea}
-                            color ="red"  
                             placeholder='Go rant here...'
                             onChange={(e) => setInputText(e.target.value)}
-                            
                         />
 
                     <Button class="main__btn"
@@ -67,19 +63,16 @@ const getOpenAiResponse = () => {
                             value={resultText}
                         />
                     </Form>
-                    
-
+            
                 </div>
+                <div><p className='p'>//Daddychill is an AI Rant Translator app that helps users communicate their
+                   thoughts and emotions in a more neutral and professional manner. Using cutting-edge 
+                   artificial intelligence technology, daddychill analyzes angry or frustrated statements and 
+                   rephrases them into a calmer, clearer tone. With daddychill, users can effectively convey their message and reduce the risk of misunderstandings. 
+                  Whether for personal or professional use, this is an indispensable tool for
+                   anyone looking to communicate their thoughts in a positive and effective way.</p></div>
             </div>
-            <div className='p'>
-              <p>//Daddychill is an AI Rant Translator app that helps users communicate their thoughts and emotions in a more neutral and 
-                professional manner. Using cutting-edge artificial intelligence technology, daddychill 
-                analyzes angry or frustrated statements and rephrases them into a calmer, clearer tone.
-                With daddychill, 
-                users can effectively convey their message and reduce the risk of misunderstandings. Whether for personal or 
-                professional use, this is an indispensable tool for anyone looking to 
-                communicate their thoughts in a positive and effective way.</p>
-            </div>
+
     </div>
     
 );
